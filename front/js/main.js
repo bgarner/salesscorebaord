eea05147d46ef60260cc5e6187d33a90b1c0653e
Brent Garner <bgarner@gmail.com>
2015-11-04 17:20:07 -0700

    cont work

--- front/js/main.js
+++ front/js/main.js
@@ -1,35 +1,78 @@
-  new Chartist.Line('#chart1', {
-    labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+//   new Chartist.Line('#chart1', {
+//     labels: ['M', 'T','W', 'Th', 'F', 'S', 'S'],
 
-       series: [
-      {
-        name: 'LY Sales',
-        data: [ 6523456, 6623456 , 7523456, 5523456, 6323456, 5923456, 6234568 ]
-      },
-      {
-        name: 'TY Sales',
-        data: [ 5523456, 4623456 , 8523456, 4823456, 4223456, 6123456, 6923456 ]
-      }
-    ]
+//     series: [
+//       {
+//         name: 'LY Sales',
+//         data: [ 6523456, 6623456 , 7523456, 5523456, 6323456, 5923456, 6234568 ]
+//       },
+//       {
+//         name: 'TY Sales',
+//         data: [ 5523456, 4623456 , 8523456, 4823456, 4223456, 6123456, 6923456 ]
+//       }
+//     ]
 
-  }, {
+//   }, {
+//   fullWidth: true,
+//   chartPadding: {
+//     top: 30,
+//     right: 40,
+//     left: 20
+//   },  
+//   axisY: {
+//     labelInterpolationFnc: function(value) {
+//       return (value / 1000000) + 'M';
+//     }
+//   },
+//   plugins: [
+//     Chartist.plugins.ctPointLabels({
+//       textAnchor: 'middle', 
+//       textSize: '10px'
+//     })
+//   ]
+  
+// });
+// 
+
+
+new Chartist.Bar('#chart1', {
+  labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+  series: [
+    [ 6523456, 6623456, 7523456, 5523456, 6323456, 5923456, 6234568 ],
+    [ 5523456, 4623456, 8523456, 4823456, 4223456, 6123456, 6923456 ]
+  ]
+}, {
   fullWidth: true,
   chartPadding: {
-    right: 40
-  },  
+    top: 30,
+    right: 25,
+    left: -20
+  },      
+  seriesBarDistance: 40,
+  axisX: {
+    offset: 60
+  },
   axisY: {
-    labelInterpolationFnc: function(value) {
-      return (value / 1000000) + 'M';
-    }
+    offset: 80,
+        labelInterpolationFnc: function(value) {
+            return (value / 1000000) + 'M';
+        },
+
+    scaleMinSpace: 15
+  }
+}).on('draw', function(data) {
+  if(data.type === 'bar') {
+    data.element.attr({
+      style: 'stroke-width: 30px'
+    }); 
   }
 });
 
-  
 
-var data = {
-    labels: ['Footwear (20%)', 'Hardgoods', 'Softgoods', 'Hockey', 'Golf', 'Licensed' ],
+var datapie = {
+    labels: ['Footwear', 'Hardgoods', 'Softgoods', 'Hockey', 'Golf', 'Licensed' ],
     series: [20, 15, 25, 8, 5, 2]
-};
+}
 
 var options = {
   labelInterpolationFnc: function(value) {
@@ -39,55 +82,90 @@ var options = {
 
 var responsiveOptions = [
   ['screen and (min-width: 640px)', {
-    chartPadding: 30,
-    labelOffset: 10,
+    chartPadding: {
+        top: 30,
+        right: 0,
+        left: 200,
+        bottom: 30
+    }, 
+    // labelOffset: 0,
     labelDirection: 'explode',
     labelInterpolationFnc: function(value) {
       return value;
     }
   }],
   ['screen and (min-width: 1024px)', {
-    labelOffset: -20,
-    chartPadding: 20
-  }]
+    labelOffset: 0,
+        chartPadding: {
+        top: 30,
+        right: 0,
+        left: 350,
+        bottom: 30
+        }
+    }]
 ];
 
-// new Chartist.Pie('#pie', data, options, responsiveOptions);
-
-new Chartist.Pie('#pie', data, options, responsiveOptions, {
-  labelInterpolationFnc: function(value) {
-    return Math.round(value / data.series.reduce(sum) * 100) + '%';
-  }
-});
+new Chartist.Pie('#pie', datapie, options, responsiveOptions);
 
 
-var databar = {
+new Chartist.Bar('#bar', {
   labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
   series: [
-    [1, 2, 4, 8, 6, -2, -1]
+    [1, 2, 4, 8.3, 6.2, -2.3, -1.9]
   ]
-};
-
-var optionsbar = {
+}, {
   high: 10,
-  low: -10
-}
+  low: -10,
+  chartPadding: {
+    right: 20,
+    top: 30,
+    left: 20
+  },   
+  plugins: [
+    Chartist.plugins.ctPointLabels({
+      textAnchor: 'middle'
+    })
+  ]
+})
 
-new Chartist.Bar('#bar', databar, optionsbar).on('draw', function(data) {
-
+.on('draw', function(data) {
   if(data.type === 'bar') {
     data.element.attr({
-      style: 'stroke: green; stroke-width: 100px'
+      style: 'stroke: green; stroke-width: 120px'
     });
 
     if(data.value.y < 0){
       data.element.attr({
-        style: 'stroke-width: 100px; stroke: red;'
+        style: 'stroke-width: 120px; stroke: red;'
       });
-    }
-    
+    } 
   }
-
-
 });
+
+var legend = $('.ct-legend');
+
+$.each(datapie.labels, function(i, val) {
+    var listItem = $('<li />')
+        .addClass('ct-series-' + i)
+        .html('<strong>' + val + '</strong>: ' + datapie.series[i] + '%')
+        .appendTo(legend);
+});
+
+// new Chartist.Bar('#bar', databar, optionsbar).on('draw', function(data) {
+
+//   if(data.type === 'bar') {
+//     data.element.attr({
+//       style: 'stroke: green; stroke-width: 120px'
+//     });
+
+//     if(data.value.y < 0){
+//       data.element.attr({
+//         style: 'stroke-width: 120px; stroke: red;'
+//       });
+//     }
+    
+//   }
+
+
+// });
 
