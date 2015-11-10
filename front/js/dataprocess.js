726d188261d8c296d888d76115c0af360770b216
Brent Garner <bgarner@gmail.com>
2015-11-10 12:18:43 -0700

    added interval for data update, rotating backgrounds

--- front/js/dataprocess.js
+++ front/js/dataprocess.js
@@ -6,140 +6,158 @@ var chartdata;
 var piedata;
 var plusminusdata;
 
-$.getJSON('http://salesscoreboardadmin.storeapps.fglsports.dmz/files/'+BANNER+'.json', function(data) {
-  console.log(data);
+var processSales = function() 
+{
 
-  var week =  data.week;
-  var banner = data.banner;
+  $.getJSON('http://salesscoreboardadmin.storeapps.fglsports.dmz/files/'+BANNER+'.json', function(data) {
+    console.log(data);
 
-  $( "#week" ).text( week );
+    var week =  data.week;
+    var banner = data.banner;
 
-    var weektotaldollars = 0;
-    var lastyeartotaldollars =0;
+    $( "#week" ).text( week );
 
-    //bar chart data
-    var actualDollars = [];  
-    var lastYearDollars = []; 
-    var plusminusdatavalues = [];
+      var weektotaldollars = 0;
+      var lastyeartotaldollars =0;
 
-    $.each(data.details, function( index, d ){
-        
-        //data table
-        $( "#"+d.day+" .day" ).text( d.day );
-        $( "#"+d.day+" .thisyear" ).text( "$" + numberWithCommas(d.data.thisyear) );
-        $( "#"+d.day+" .lastyear" ).text( "$" + numberWithCommas(d.data.lastyear) );
+      //bar chart data
+      var actualDollars = [];  
+      var lastYearDollars = []; 
+      var plusminusdatavalues = [];
 
-        var diff = parseInt(d.data.thisyear) - parseInt(d.data.lastyear);
-        var percentage = (diff / d.data.lastyear) * 100;
-        percentage = 100 + parseFloat(percentage);
-        percentage = Math.round(percentage * 100) / 100;
-        $( "#"+d.day+" .percentage" ).text( percentage +"%" );
+      $.each(data.details, function( index, d ){
+          
+          //data table
+          $( "#"+d.day+" .day" ).text( d.day );
+          $( "#"+d.day+" .thisyear" ).text( "$" + numberWithCommas(d.data.thisyear) );
+          $( "#"+d.day+" .lastyear" ).text( "$" + numberWithCommas(d.data.lastyear) );
 
-        weektotaldollars = weektotaldollars + parseInt(d.data.thisyear);
-        lastyeartotaldollars = lastyeartotaldollars + parseInt(d.data.lastyear);
-        
-        //add dollars to 
-        actualDollars.push( parseInt(d.data.thisyear) );
-        lastYearDollars.push( parseInt(d.data.lastyear) );
+          var diff = parseInt(d.data.thisyear) - parseInt(d.data.lastyear);
+          var percentage = (diff / d.data.lastyear) * 100;
+          percentage = 100 + parseFloat(percentage);
+          percentage = Math.round(percentage * 100) / 100;
+          $( "#"+d.day+" .percentage" ).text( percentage +"%" );
 
-        var plusminus = percentage - 100;
-        plusminusdatavalues.push( parseFloat(plusminus) );
+          weektotaldollars = weektotaldollars + parseInt(d.data.thisyear);
+          lastyeartotaldollars = lastyeartotaldollars + parseInt(d.data.lastyear);
+          
+          //add dollars to 
+          actualDollars.push( parseInt(d.data.thisyear) );
+          lastYearDollars.push( parseInt(d.data.lastyear) );
 
-    });
+          var plusminus = percentage - 100;
+          plusminusdatavalues.push( parseFloat(plusminus) );
 
-    //week totals
-    $( ".week-total-value" ).text( "$" + numberWithCommas(weektotaldollars) )
+      });
 
-    //total %
-    var diff = weektotaldollars - lastyeartotaldollars;
-    var totalcompperc = (diff / lastyeartotaldollars) * 100;
-    weekpercentage = 100 + parseFloat(totalcompperc);
-    weekpercentage = Math.round(weekpercentage * 100) / 100;
-    $( ".week-total-comp-value" ).text( weekpercentage + "%" );    
+      //week totals
+      $( ".week-total-value" ).text( "$" + numberWithCommas(weektotaldollars) )
+
+      //total %
+      var diff = weektotaldollars - lastyeartotaldollars;
+      var totalcompperc = (diff / lastyeartotaldollars) * 100;
+      weekpercentage = 100 + parseFloat(totalcompperc);
+      weekpercentage = Math.round(weekpercentage * 100) / 100;
+      $( ".week-total-comp-value" ).text( weekpercentage + "%" );    
 
 
 
-    chartdata = {
-      labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
-      series: [
-        actualDollars,
-        lastYearDollars
-      ]
-    }
-
-    plusminusdata = {
-      labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
-      series: [
-            plusminusdatavalues
+      chartdata = {
+        labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        series: [
+          actualDollars,
+          lastYearDollars
         ]
-    }
-})
-
-.done( function(){
-
-    new Chartist.Bar('#chart1', chartdata, chartoptions)
-    .on('draw', function(data) {
-  
-      if(data.type === 'bar') {
-        data.element.attr({
-          style: 'stroke: green; stroke-width: 30px'
-        }); 
       }
-      if(data.seriesIndex == 1){
-       data.element.attr({
-          style: 'stroke: #eee; stroke-width: 30px'
-        });  
-      }
-    });
 
-    new Chartist.Bar('#bar', plusminusdata, plusminusoptions)
-    .on('draw', function(data) {
-      if(data.type === 'bar') {
-        data.element.attr({
-          style: 'stroke: green; stroke-width: 120px'
+      plusminusdata = {
+        labels: ['Mon', 'Tues','Wed', 'Thurs', 'Fri', 'Sat', 'Sun'],
+        series: [
+              plusminusdatavalues
+          ]
+      }
+  })
+
+  .done( function(){
+
+      new Chartist.Bar('#chart1', chartdata, chartoptions)
+      .on('draw', function(data) {
+    
+        if(data.type === 'bar') {
+          data.element.attr({
+            style: 'stroke: green; stroke-width: 30px'
+          }); 
+        }
+        if(data.seriesIndex == 1){
+         data.element.attr({
+            style: 'stroke: #eee; stroke-width: 30px'
+          });  
+        }
+      });
+
+      new Chartist.Bar('#bar', plusminusdata, plusminusoptions)
+      .on('draw', function(data) {
+        if(data.type === 'bar') {
+          data.element.attr({
+            style: 'stroke: green; stroke-width: 120px'
+          });
+
+          if(data.value.y < 0){
+            data.element.attr({
+              style: 'stroke-width: 120px; stroke: red;'
+            });
+          } 
+        }
+      });
+  });
+
+}
+
+var processPie = function()
+{
+    var pieDollarsArray = [];
+    var pieLabelsArray = [];
+    $.getJSON('http://salesscoreboardadmin.storeapps.fglsports.dmz/files/pie/'+BANNER+'.json', function(data) {
+      
+        $.each(data.details, function( index, d ){
+            
+            pieLabelsArray.push( d.category ); 
+            pieDollarsArray.push( parseInt(d.value));
+
         });
 
-        if(data.value.y < 0){
-          data.element.attr({
-            style: 'stroke-width: 120px; stroke: red;'
-          });
-        } 
-      }
+        piedata = {
+            labels: pieLabelsArray,
+            series: pieDollarsArray
+        }
+
+    })
+    .done( function() {
+        $( ".ct-legend" ).empty();
+        new Chartist.Pie('#pie', piedata, pieoptions, pieresponsiveOptions);
+
+        var legend = $('.ct-legend');
+        $.each(piedata.labels, function(i, val) {
+            var listItem = $('<li />')
+                .addClass('ct-series-' + i)
+                .html('<strong>' + val + '</strong>: $' + numberWithCommas(piedata.series[i]))
+                .appendTo(legend);
+        });
     });
-});
+}
 
-
-var pieDollarsArray = [];
-var pieLabelsArray = [];
-$.getJSON('http://salesscoreboardadmin.storeapps.fglsports.dmz/files/pie/'+BANNER+'.json', function(data) {
-  
-    $.each(data.details, function( index, d ){
-        
-        pieLabelsArray.push( d.category ); 
-        pieDollarsArray.push( parseInt(d.value));
-
-    });
-
-    piedata = {
-        labels: pieLabelsArray,
-        series: pieDollarsArray
-    }
-
-})
-.done( function() {
-
-    new Chartist.Pie('#pie', piedata, pieoptions, pieresponsiveOptions);
-
-    var legend = $('.ct-legend');
-    $.each(piedata.labels, function(i, val) {
-        var listItem = $('<li />')
-            .addClass('ct-series-' + i)
-            .html('<strong>' + val + '</strong>: $' + numberWithCommas(piedata.series[i]))
-            .appendTo(legend);
-    });
-
-
-
-});
  
+
+$(document).ready(function(){ 
+
+  processSales();
+  processPie();
+
+  setInterval(function(){ 
+    console.log("go!");
+    processSales(); 
+    processPie();
+  },5000);
+
+}); 
 
